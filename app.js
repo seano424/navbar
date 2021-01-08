a58a8b3faa3c81b8e6b71bf051262dd75a049b05
Sean OReilly <soreilly424@gmail.com>
2021-01-08 16:01:11 -0500

    v2 added the hover remove functionality to the initial part

--- app.js
+++ app.js
@@ -9,5 +9,26 @@ const nav = document.querySelector('#main');
       document.body.classList.remove('fixed-nav')
     }
   }
+  
+  const blog = document.querySelector('.blog');
+  const dropdown = document.querySelector('.dropdown');
+  const body = document.querySelector('body');
+  function handleEnter() {
+    console.log('hello');
+    dropdown.classList.add('trigger-enter');
+    setTimeout(() => dropdown.classList.contains('trigger-enter') && dropdown.classList.add('trigger-enter-active'), 150)
 
+  }
+  function handleLeave(e) {
+    console.log(e.target);
+    dropdown.classList.remove('trigger-enter', 'trigger-enter-active');
+    // if(!e.target.classList.contains('dropdown')) {
+    // }
+  }
+
+  blog.addEventListener('mouseenter', handleEnter);
+  blog.addEventListener('mouseleave', handleLeave)
+  
+  
   window.addEventListener('scroll', fixNav);
+
